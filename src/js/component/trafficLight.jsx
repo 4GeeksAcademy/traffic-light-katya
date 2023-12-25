1a8a2730b0519703cdc3b3cab06df2d40a936099
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-12-25 18:05:42 +0000

    Design finished

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -9,33 +9,37 @@ const TrafficLight = () => {
   }
 
   return (
-    <div className="d-flex justify-content-center m-3">
-      {" "}
-      <div className="col-1 d-flex flex-column mb-3 justify-content-center bg-black rounded">
-        <button
-          onClick={() => clickHandler("red")}
-          type="button"
-          className={`btn btn-danger m-3 rounded-circle ${
-            color == "red" ? "glow-red" : ""
-          }`}
-          style={{ height: "100px" }}
-        ></button>
-        <button
-          onClick={() => clickHandler("yellow")}
-          type="button"
-          className={`btn btn-warning m-3 rounded-circle ${
-            color == "yellow" ? "glow-yellow" : ""
-          }`}
-          style={{ height: "100px" }}
-        ></button>
-        <button
-          onClick={() => clickHandler("green")}
-          type="button"
-          className={`btn btn-success m-3 rounded-circle ${
-            color == "green" ? "glow-green" : ""
-          }`}
-          style={{ height: "100px" }}
-        ></button>
+    <div>
+      <div className="row d-flex justify-content-center">
+        <div className="bg-black" style={{height: "100px", width: "30px"}}></div>{" "}
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-1 d-flex flex-column justify-content-center bg-black rounded">
+          <button
+            onClick={() => clickHandler("red")}
+            type="button"
+            className={`btn btn-danger m-3 rounded-circle ${
+              color == "red" ? "glow-red" : ""
+            }`}
+            style={{ height: "100px" }}
+          ></button>
+          <button
+            onClick={() => clickHandler("yellow")}
+            type="button"
+            className={`btn btn-warning m-3 rounded-circle ${
+              color == "yellow" ? "glow-yellow" : ""
+            }`}
+            style={{ height: "100px" }}
+          ></button>
+          <button
+            onClick={() => clickHandler("green")}
+            type="button"
+            className={`btn btn-success m-3 rounded-circle ${
+              color == "green" ? "glow-green" : ""
+            }`}
+            style={{ height: "100px" }}
+          ></button>
+        </div>
       </div>
     </div>
   );
